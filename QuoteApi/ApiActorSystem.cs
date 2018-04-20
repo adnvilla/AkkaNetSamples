0b9bb9f294bb5011e254ae7daf59a5d3216d25fa
Adrian Villafaña <paulo.gonzalez@pricetravel.com>
2018-04-19 19:03:22 -0500

    Cambios para utilizar en api

---
+++ QuoteApi/ApiActorSystem.cs
@@ -0,0 +1,122 @@
+﻿
+using Akka.Actor;
+using Akka.Configuration;
+using Akka.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using QuoteNetStandardShared;
+
+namespace QuoteApi
+{
+    public class ApiActorSystem
+    {
+        private static ActorSystem _actorSystem;
+
+        static ApiActorSystem()
+        {
+            Config = ConfigurationFactory.ParseString(@"
+                    akka {
+                        log-config-on-start = on
+                        stdout-loglevel = DEBUG
+                        loglevel = DEBUG
+                        actor {
+                            provider = ""Akka.Remote.RemoteActorRefProvider, Akka.Remote""
+
+                            debug {
+                              receive = on
+                              autoreceive = off
+                              lifecycle = off
+                              event-stream = off
+                              unhandled = on
+                            }
+
+                            deployment {
+                                /localactor {
+                                    router = round-robin-pool
+                                    nr-of-instances = 5
+                                    resizer {
+                                        enabled = on
+                                        lower-bound = 1
+                                        upper-bound = 10
+                                    }
+                                }
+                                /remoteactor {
+                                    router = round-robin-pool
+                                    nr-of-instances = 5
+                                    remote = ""akka.tcp://QuoteRemote@localhost:8080""
+                                    resizer {
+                                        enabled = on
+                                        lower-bound = 1
+                                        upper-bound = 10
+                                    }
+                                }
+                            }
+                        }
+                        remote {
+                            dot-netty.tcp {
+		                        port = 8090
+		                        hostname = localhost
+                            }
+                        }
+                    }
+                    ");
+        }
+
+        public static Config Config { get; set; }
+
+        private static object _obj = new object();
+        public static ActorSystem ActorSystem
+        {
+            get
+            {
+                if (_actorSystem == null)
+                {
+                    lock (_obj)
+                    {
+                        if (_actorSystem == null)
+                        {
+                            Initialize();
+                        }
+                    }
+                }
+
+                return _actorSystem;
+            }
+        }
+
+        private static IActorRef _remote;
+
+
+        private static object _objr = new object();
+        public static IActorRef Remote
+        {
+            get
+            {
+                if (_remote == null)
+                {
+                    lock (_objr)
+                    {
+                        if (_remote == null)
+                        {
+                            InitializeRemote();
+                        }
+                    }
+                }
+
+                return _remote;
+            }
+        }
+
+        private static void InitializeRemote()
+        {
+            _remote = ApiActorSystem.ActorSystem.ActorOf(Props.Create<Aggregator<Response>>("Remote").WithRouter(FromConfig.Instance), "remoteactor");
+        }
+
+        private static void Initialize()
+        {
+            _actorSystem = ActorSystem.Create("Quote", Config);
+        }
+    }
+}
---
+++ QuoteApi/Controllers/ValuesController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Akka.Actor;
+using Akka.Routing;
+using Microsoft.AspNetCore.Mvc;
+using QuoteNetStandardShared;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QuoteApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var results = new List<string>();
+
+            using (var system = ActorSystem.Create(Guid.NewGuid().ToString(), ApiActorSystem.Config))
+            {
+                // DIspatcher && Aggregator
+                var remote =
+                    system.ActorOf(Props.Create<Aggregator<Response>>(Guid.NewGuid().ToString()).WithRouter(FromConfig.Instance),
+                        "remoteactor");
+
+                var r = remote.Ask<AggregatedReply<Response>>(new Request { }).Result;
+
+                results.Add("Response Count " + r.Replies.Count + " Hotels: " + r.Replies.SelectMany(y => y.Hotel).Count());
+            }
+
+
+            // DIspatcher && Aggregator
+            //var remote =
+            //    ApiActorSystem.ActorSystem.ActorOf(Props.Create<Aggregator<Response>>("Remote").WithRouter(FromConfig.Instance),
+            //        "remoteactor");
+
+            //var r = remote.Ask<AggregatedReply<Response>>(new Request { });
+
+            //r.ContinueWith(x =>
+            //{
+            //    results.Add("Response Count " + x.Result.Replies.Count + " Hotels: " +
+            //                x.Result.Replies.SelectMany(y => y.Hotel).Count());
+            //});
+
+
+            //TODO
+            //    var r = ApiActorSystem.Remote.Ask<AggregatedReply<Response>>(new Request { }).Result;
+
+            //results.Add("Response Count " + r.Replies.Count + " Hotels: " +
+            //            r.Replies.SelectMany(y => y.Hotel).Count());
+
+            return results;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
